786fc70e24d9d11d8d3163f46b1a98e9280733bb
Nazir Louis <nazirlouis38@gmail.com>
2021-12-08 17:57:36 -0500

    Tesla Clone

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Home from "./componets/Home";
+import Header from "./componets/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
